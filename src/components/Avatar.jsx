66e88573f28078a4a6a78b6a594fff66868195f5
NamaN Bhalani <bhalaninaman007@gmail.com>
2024-04-01 19:12:18 +0530

    Avatar Fix

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -14,7 +14,7 @@ export function Avatar(props) {
     cursorFollow: false,
   });
   const group = useRef();
-  const { nodes, materials } = useGLTF("models/646d9dcdc8a5f5bddbfac913.glb");
+  const { nodes, materials } = useGLTF("models/6603efe1676c04edeb1ee00b.glb");
 
   const { animations: typingAnimation } = useFBX("animations/Typing.fbx");
   const { animations: standingAnimation } = useFBX(
@@ -58,80 +58,86 @@ export function Avatar(props) {
 
   return (
     <group {...props} ref={group} dispose={null}>
-      <group>
-        <primitive object={nodes.Hips} />
-        <skinnedMesh
-          frustumCulled={false}
-          geometry={nodes.Wolf3D_Body.geometry}
-          material={materials.Wolf3D_Body}
-          skeleton={nodes.Wolf3D_Body.skeleton}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-          material={materials.Wolf3D_Outfit_Bottom}
-          skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-          material={materials.Wolf3D_Outfit_Footwear}
-          skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          geometry={nodes.Wolf3D_Outfit_Top.geometry}
-          material={materials.Wolf3D_Outfit_Top}
-          skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          geometry={nodes.Wolf3D_Hair.geometry}
-          material={materials.Wolf3D_Hair}
-          skeleton={nodes.Wolf3D_Hair.skeleton}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          name="EyeLeft"
-          geometry={nodes.EyeLeft.geometry}
-          material={materials.Wolf3D_Eye}
-          skeleton={nodes.EyeLeft.skeleton}
-          morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-          morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          name="EyeRight"
-          geometry={nodes.EyeRight.geometry}
-          material={materials.Wolf3D_Eye}
-          skeleton={nodes.EyeRight.skeleton}
-          morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-          morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          name="Wolf3D_Head"
-          geometry={nodes.Wolf3D_Head.geometry}
-          material={materials.Wolf3D_Skin}
-          skeleton={nodes.Wolf3D_Head.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
-        />
-        <skinnedMesh
-          frustumCulled={false}
-          name="Wolf3D_Teeth"
-          geometry={nodes.Wolf3D_Teeth.geometry}
-          material={materials.Wolf3D_Teeth}
-          skeleton={nodes.Wolf3D_Teeth.skeleton}
-          morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-          morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
-        />
-      </group>
-    </group>
+      <group rotation-x={-Math.PI /2}>
+    <primitive object={nodes.Hips} />
+    <skinnedMesh
+      name="EyeLeft"
+      geometry={nodes.EyeLeft.geometry}
+      material={materials.Wolf3D_Eye}
+      skeleton={nodes.EyeLeft.skeleton}
+      morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+      morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      name="EyeRight"
+      geometry={nodes.EyeRight.geometry}
+      material={materials.Wolf3D_Eye}
+      skeleton={nodes.EyeRight.skeleton}
+      morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+      morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      name="Wolf3D_Head"
+      geometry={nodes.Wolf3D_Head.geometry}
+      material={materials.Wolf3D_Skin}
+      skeleton={nodes.Wolf3D_Head.skeleton}
+      morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+      morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      name="Wolf3D_Teeth"
+      geometry={nodes.Wolf3D_Teeth.geometry}
+      material={materials.Wolf3D_Teeth}
+      skeleton={nodes.Wolf3D_Teeth.skeleton}
+      morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+      morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      geometry={nodes.Wolf3D_Hair.geometry}
+      material={materials.Wolf3D_Hair}
+      skeleton={nodes.Wolf3D_Hair.skeleton}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      geometry={nodes.Wolf3D_Glasses.geometry}
+      material={materials.Wolf3D_Glasses}
+      skeleton={nodes.Wolf3D_Glasses.skeleton}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      geometry={nodes.Wolf3D_Body.geometry}
+      material={materials.Wolf3D_Body}
+      skeleton={nodes.Wolf3D_Body.skeleton}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+      material={materials.Wolf3D_Outfit_Bottom}
+      skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+      material={materials.Wolf3D_Outfit_Footwear}
+      skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+      frustumCulled={false}
+      />
+    <skinnedMesh
+      geometry={nodes.Wolf3D_Outfit_Top.geometry}
+      material={materials.Wolf3D_Outfit_Top}
+      skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+  frustumCulled={false}
+      />
+       </group>
+   </group>
   );
 }
 
-useGLTF.preload("models/646d9dcdc8a5f5bddbfac913.glb");
+useGLTF.preload("models/6603efe1676c04edeb1ee00b.glb");
 useFBX.preload("animations/Typing.fbx");
 useFBX.preload("animations/Standing Idle.fbx");
 useFBX.preload("animations/Falling Idle.fbx");
